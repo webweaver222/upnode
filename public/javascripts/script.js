47e25f6bc3246e3f6d80229c21069c53ce2e2bc3
Alex Teisheira <fukirx@gmail.com>
2019-08-25 21:43:46 +0300

    frontpage flipper

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,1 +1,66 @@
-console.log('hello form client js');
+
+
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+  }
+  
+  ready(() => { 
+    console.log('hello form client js');
+
+    document.getElementById('vid').play();
+
+    let flip   = document.querySelector(".theflip").style;
+    let upside = document.querySelector(".signup");
+    let inside = document.querySelector(".signin");
+    let back_arrow   = document.querySelector(".back-arrow")
+
+
+    document.querySelector(".up").addEventListener("click", (e) => { 
+        
+        flip.transform = 'rotateY(180deg)';
+        inside.style.display = 'none';
+        setTimeout(() => {
+          back_arrow.style.display = 'inline';
+        }, 600)
+         
+        
+
+        
+    });
+
+    document.querySelector(".in").addEventListener("click", (e) => { 
+       
+        flip.transform = 'rotateY(180deg)';
+        upside.style.display = 'none';
+        setTimeout(() => {
+          back_arrow.style.display = 'inline';
+        }, 400)
+      
+    });
+
+    back_arrow.addEventListener("click", (e) => { 
+
+      if (inside.style.display == 'none') {
+
+        flip.transform = 'rotateY(360deg)';
+      
+        setTimeout(() => {
+         inside.style.display = 'flex'
+        }, 400)
+     
+      } else {
+
+        flip.transform = 'rotateY(360deg)';
+        
+        setTimeout(() => {
+          upside.style.display = 'flex'
+        }, 400)
+     
+      }
+
+      
+      back_arrow.style.display = 'none';
+  });
+
+  });
